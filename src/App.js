7f1b815c50554dbf7d1e9772d202b79cf59c147a
MacBook Pro <macbookpro@MacBook-Pro-de-MacBook.local>
2023-09-06 19:10:12 +0100

    created  components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { Navbar } from "./component/Navbar";
+import { Hero } from "./component/Hero";
+import { About } from "./component/About";
+import { Services } from "./component/Services";
+import { Tours } from "./component/Tours";
+import { Footer } from "./component/Footer";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+<>
+     <Navbar />
+     <Hero />
+     <About />
+     <Services />
+     <Tours />
+     <Footer /> 
+ </>     
+)     
 }
 
 export default App;
---
+++ src/component/Navbar.js
@@ -0,0 +1,78 @@
+import React from 'react'
+// import { logo } from '../images/logo.svg'
+export const Navbar = () => {
+  return (
+    <div>
+      <nav className='navbar'>
+        <div className='nav-center'>
+          <div class='nav-header'>
+            <img src='' class='nav-logo' alt='myTravel' />
+            <button type='button' class='nav-toggle' id='nav-toggle'>
+              <i class='fas fa-bars'></i>
+            </button>
+          </div>
+
+          <ul class='nav-links' id='nav-links'>
+            <li>
+              <a href='#home' class='nav-link'>
+                {' '}
+                home{' '}
+              </a>
+            </li>
+
+            <li>
+              <a href='#about' class='nav-link'>
+                {' '}
+                about{' '}
+              </a>
+            </li>
+
+            <li>
+              <a href='#services' class='nav-link'>
+                {' '}
+                services{' '}
+              </a>
+            </li>
+
+            <li>
+              <a href='#tours' class='nav-link'>
+                {' '}
+                tours
+              </a>
+            </li>
+          </ul>
+
+          <ul class='nav-icons'>
+            <li>
+              <a
+                href='https://www.twitter.com'
+                target='_blank'
+                class='nav-icon'
+              >
+                <i class='fab fa-facebook'></i>
+              </a>
+            </li>
+            <li>
+              <a
+                href='https://www.twitter.com'
+                target='_blank'
+                class='nav-icon'
+              >
+                <i class='fab fa-twitter'></i>
+              </a>
+            </li>
+            <li>
+              <a
+                href='https://www.twitter.com'
+                target='_blank'
+                class='nav-icon'
+              >
+                <i class='fab fa-squarespace'></i>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+  )
+}
